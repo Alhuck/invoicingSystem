6acc37ed203d7a6cdf0382e028df2f3f1b4a83b8
Alhuck <alhucka@gmail.com>
2020-01-15 22:52:04 -0500

    Tried and commented out the different way to initialize the component with currency formatted values

--- src/main/webapp/app/shared/directives/currency-formatter.directive.ts
+++ src/main/webapp/app/shared/directives/currency-formatter.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer, Input, HostListener, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer, Input, HostListener, OnInit, HostBinding } from '@angular/core';
 import { NgModel } from '@angular/forms';
 
 @Directive({
@@ -11,8 +11,20 @@ export class CurrencyFormatterDirective implements OnInit {
   constructor(public model: NgModel, private el: ElementRef, private renderer: Renderer) {}
 
   ngOnInit(): void {
-    this.changeCurrencyFormat();
+    // this.changeCurrencyFormat();
+    // this.el.nativeElement.value = 100;
+    // // console.log(this.el.nativeElement);
+    // // eslint-disable-next-line no-console
+    // console.log(this.el.nativeElement.value);
   }
+
+  // @HostBinding('value') inp: any;
+  // onInputChange() : void {
+  //   const value = this.ngModel;
+  //   // this.ngModel.update(Number(value).toFixed(2));
+  //   this.model.viewToModelUpdate(value);
+  //   this.model.valueAccessor ? this.model.valueAccessor.writeValue(Number(value).toFixed(2)) : 0;
+  // }
 
   @HostListener('blur', ['$event'])
   changeCurrencyFormat(): void {
@@ -21,7 +33,6 @@ export class CurrencyFormatterDirective implements OnInit {
     this.model.viewToModelUpdate(value);
     this.model.valueAccessor ? this.model.valueAccessor.writeValue(Number(value).toFixed(2)) : 0;
 
-    // eslint-disable-next-line no-console
     // console.log();
   }
 }
