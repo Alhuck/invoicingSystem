78fbee02c3791da2ae392e94eda79ac9a13fb40f
Alhuck <alhucka@gmail.com>
2020-01-19 19:42:45 -0500

    Graphql issue fixed by adding component annotations in the GraphQLQueryResolver component

--- src/main/java/com/alhuck/invoice/graphql/query/UserQuery.java
+++ src/main/java/com/alhuck/invoice/graphql/query/UserQuery.java
@@ -1,24 +1,30 @@
 package com.alhuck.invoice.graphql.query;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
 
 import com.alhuck.invoice.domain.User;
+import com.alhuck.invoice.repository.UserRepository;
 import com.alhuck.invoice.service.UserService;
 import com.alhuck.invoice.service.dto.UserDTO;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 
-public class UserQuery implements GraphQLQueryResolver { 
+@Component
+public class UserQuery implements GraphQLQueryResolver {
 
-	@Autowired
-	private UserService userService;
-	
-	public Page<UserDTO> getUsers() {
-		return this.userService.getAllManagedUsers(null);
-	}
-	public Optional<User> getUser(String id) {
-		return this.userService.getUserWithAuthorities(id);
-	}
+    @Autowired
+    private UserRepository userService;
+
+    public List<User> getUsers() {
+        return (List<User>) this.userService.findAllByLoginNot(null, "anonymoususer")
+            .getContent();
+    }
+
+    public Optional<User> getUser(String id) {
+        return this.userService.findOneByLogin(id);
+    }
 }
